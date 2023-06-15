59fcb42869b4ca8451599cca00dc7795ef41848a
longmingpei <longmingpei@hczy.com>
2023-06-15 17:37:36 +0800

    [U]更新路由

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,8 +1,8 @@
 /*
- * @Description:
+ * @Description:  react路由
  * @Author: qingZhiKing
  * @Date: 2023-06-15 14:29:32
- * @LastEditTime: 2023-06-15 17:05:39
+ * @LastEditTime: 2023-06-15 17:36:45
  * @LastEditors: qingZhiKing
  */
 
